8b99a0f051ab1f01368776a2a11498589f8996a0
ilyankin <lomernap881@gmail.com>
2026-07-04 16:00:27 +0300

    refactor(common): extract JPA auditing into AuditConfig

---
+++ src/main/java/com/izenkyt/searchindexmanager/common/AuditConfig.java
@@ -0,0 +1,9 @@
+package com.izenkyt.searchindexmanager.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditConfig {
+}
