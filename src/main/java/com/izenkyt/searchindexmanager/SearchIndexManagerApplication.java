8745d26f7f58f830edaea985a863d64c6c28c6b5
ilyankin <lomernap881@gmail.com>
2026-07-04 12:09:24 +0300

    Init commit

---
+++ src/main/java/com/izenkyt/searchindexmanager/SearchIndexManagerApplication.java
@@ -0,0 +1,13 @@
+package com.izenkyt.searchindexmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchIndexManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchIndexManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/izenkyt/searchindexmanager/SearchIndexManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izenkyt.searchindexmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchIndexManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
